02678bd1984f86b9cd61e4169887bf561ee3c60a
Hieu Tran <vuhieu91@gmail.com>
2021-06-27 15:39:24 +0700

    ! Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "826ef6650e2559723df4cdc8f9e7ec48",
-"index.html": "b4491648f122e358aa671797d69bfe01",
-"/": "b4491648f122e358aa671797d69bfe01",
+"index.html": "48ee051bd0cbfd7a6c17dd18d6eca9ef",
+"/": "48ee051bd0cbfd7a6c17dd18d6eca9ef",
 "main.dart.js": "ea23e435d69770734490819cd45cff79",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
